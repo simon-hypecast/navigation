c18f0d78183a6c6292c422f47c996d543ef5a6b2
Simon Kapell <simon@hype1000.de>
2022-03-08 21:47:44 +0100

    add nestedRoutes to Home

--- lib/routes/router.gr.dart
+++ lib/routes/router.gr.dart
@@ -10,75 +10,82 @@
 //
 // ignore_for_file: type=lint
 
-import 'package:auto_route/auto_route.dart' as _i8;
-import 'package:flutter/material.dart' as _i9;
+import 'package:auto_route/auto_route.dart' as _i9;
+import 'package:flutter/material.dart' as _i10;
 import 'package:navigation/pages/favorite_page.dart' as _i5;
 import 'package:navigation/pages/favorite_screen.dart' as _i3;
-import 'package:navigation/pages/home_page.dart' as _i4;
+import 'package:navigation/pages/home_page.dart' as _i7;
+import 'package:navigation/pages/home_screen.dart' as _i4;
 import 'package:navigation/pages/login_page.dart' as _i1;
-import 'package:navigation/pages/nested_page.dart' as _i7;
+import 'package:navigation/pages/nested_page.dart' as _i8;
 import 'package:navigation/pages/root_page.dart' as _i2;
 import 'package:navigation/pages/under_page.dart' as _i6;
 
-class AppRouter extends _i8.RootStackRouter {
-  AppRouter([_i9.GlobalKey<_i9.NavigatorState>? navigatorKey])
+class AppRouter extends _i9.RootStackRouter {
+  AppRouter([_i10.GlobalKey<_i10.NavigatorState>? navigatorKey])
       : super(navigatorKey);
 
   @override
-  final Map<String, _i8.PageFactory> pagesMap = {
+  final Map<String, _i9.PageFactory> pagesMap = {
     LoginPage.name: (routeData) {
       final args =
           routeData.argsAs<LoginPageArgs>(orElse: () => const LoginPageArgs());
-      return _i8.AdaptivePage<dynamic>(
+      return _i9.AdaptivePage<dynamic>(
           routeData: routeData, child: _i1.LoginPage(key: args.key));
     },
     RootPage.name: (routeData) {
-      return _i8.AdaptivePage<dynamic>(
+      return _i9.AdaptivePage<dynamic>(
           routeData: routeData, child: const _i2.RootPage());
     },
     FavoriteScreen.name: (routeData) {
-      return _i8.AdaptivePage<dynamic>(
+      return _i9.AdaptivePage<dynamic>(
           routeData: routeData, child: const _i3.FavoriteScreen());
     },
-    HomePage.name: (routeData) {
-      return _i8.AdaptivePage<dynamic>(
-          routeData: routeData, child: const _i4.HomePage());
+    HomeScreen.name: (routeData) {
+      return _i9.AdaptivePage<dynamic>(
+          routeData: routeData, child: const _i4.HomeScreen());
     },
     FavoritePageRoute.name: (routeData) {
-      return _i8.AdaptivePage<dynamic>(
+      return _i9.AdaptivePage<dynamic>(
           routeData: routeData, child: const _i5.FavoritePage());
     },
     UnderPage.name: (routeData) {
-      return _i8.AdaptivePage<dynamic>(
+      return _i9.AdaptivePage<dynamic>(
           routeData: routeData, child: const _i6.UnderPage());
+    },
+    HomePageRoute.name: (routeData) {
+      return _i9.AdaptivePage<dynamic>(
+          routeData: routeData, child: const _i7.HomePage());
     },
     NestedPage.name: (routeData) {
       final args = routeData.argsAs<NestedPageArgs>(
           orElse: () => const NestedPageArgs());
-      return _i8.AdaptivePage<dynamic>(
-          routeData: routeData, child: _i7.NestedPage(key: args.key));
+      return _i9.AdaptivePage<dynamic>(
+          routeData: routeData, child: _i8.NestedPage(key: args.key));
     }
   };
 
   @override
-  List<_i8.RouteConfig> get routes => [
-        _i8.RouteConfig(LoginPage.name, path: '/'),
-        _i8.RouteConfig(RootPage.name, path: '/root-page', children: [
-          _i8.RouteConfig(FavoriteScreen.name,
+  List<_i9.RouteConfig> get routes => [
+        _i9.RouteConfig(LoginPage.name, path: '/'),
+        _i9.RouteConfig(RootPage.name, path: '/root-page', children: [
+          _i9.RouteConfig(FavoriteScreen.name,
               path: 'favorite',
               parent: RootPage.name,
               children: [
-                _i8.RouteConfig(FavoritePageRoute.name,
+                _i9.RouteConfig(FavoritePageRoute.name,
                     path: '', parent: FavoriteScreen.name),
-                _i8.RouteConfig(UnderPage.name,
+                _i9.RouteConfig(UnderPage.name,
                     path: 'underPage/*', parent: FavoriteScreen.name)
               ]),
-          _i8.RouteConfig(HomePage.name,
-              path: 'home-page',
+          _i9.RouteConfig(HomeScreen.name,
+              path: 'home',
               parent: RootPage.name,
               children: [
-                _i8.RouteConfig(NestedPage.name,
-                    path: 'nested-page', parent: HomePage.name)
+                _i9.RouteConfig(HomePageRoute.name,
+                    path: '', parent: HomeScreen.name),
+                _i9.RouteConfig(NestedPage.name,
+                    path: 'nestedPage/*', parent: HomeScreen.name)
               ])
         ])
       ];
@@ -86,8 +93,8 @@ class AppRouter extends _i8.RootStackRouter {
 
 /// generated route for
 /// [_i1.LoginPage]
-class LoginPage extends _i8.PageRouteInfo<LoginPageArgs> {
-  LoginPage({_i9.Key? key})
+class LoginPage extends _i9.PageRouteInfo<LoginPageArgs> {
+  LoginPage({_i10.Key? key})
       : super(LoginPage.name, path: '/', args: LoginPageArgs(key: key));
 
   static const String name = 'LoginPage';
@@ -96,7 +103,7 @@ class LoginPage extends _i8.PageRouteInfo<LoginPageArgs> {
 class LoginPageArgs {
   const LoginPageArgs({this.key});
 
-  final _i9.Key? key;
+  final _i10.Key? key;
 
   @override
   String toString() {
@@ -106,8 +113,8 @@ class LoginPageArgs {
 
 /// generated route for
 /// [_i2.RootPage]
-class RootPage extends _i8.PageRouteInfo<void> {
-  const RootPage({List<_i8.PageRouteInfo>? children})
+class RootPage extends _i9.PageRouteInfo<void> {
+  const RootPage({List<_i9.PageRouteInfo>? children})
       : super(RootPage.name, path: '/root-page', initialChildren: children);
 
   static const String name = 'RootPage';
@@ -115,25 +122,25 @@ class RootPage extends _i8.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i3.FavoriteScreen]
-class FavoriteScreen extends _i8.PageRouteInfo<void> {
-  const FavoriteScreen({List<_i8.PageRouteInfo>? children})
+class FavoriteScreen extends _i9.PageRouteInfo<void> {
+  const FavoriteScreen({List<_i9.PageRouteInfo>? children})
       : super(FavoriteScreen.name, path: 'favorite', initialChildren: children);
 
   static const String name = 'FavoriteScreen';
 }
 
 /// generated route for
-/// [_i4.HomePage]
-class HomePage extends _i8.PageRouteInfo<void> {
-  const HomePage({List<_i8.PageRouteInfo>? children})
-      : super(HomePage.name, path: 'home-page', initialChildren: children);
+/// [_i4.HomeScreen]
+class HomeScreen extends _i9.PageRouteInfo<void> {
+  const HomeScreen({List<_i9.PageRouteInfo>? children})
+      : super(HomeScreen.name, path: 'home', initialChildren: children);
 
-  static const String name = 'HomePage';
+  static const String name = 'HomeScreen';
 }
 
 /// generated route for
 /// [_i5.FavoritePage]
-class FavoritePageRoute extends _i8.PageRouteInfo<void> {
+class FavoritePageRoute extends _i9.PageRouteInfo<void> {
   const FavoritePageRoute() : super(FavoritePageRoute.name, path: '');
 
   static const String name = 'FavoritePageRoute';
@@ -141,18 +148,26 @@ class FavoritePageRoute extends _i8.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i6.UnderPage]
-class UnderPage extends _i8.PageRouteInfo<void> {
+class UnderPage extends _i9.PageRouteInfo<void> {
   const UnderPage() : super(UnderPage.name, path: 'underPage/*');
 
   static const String name = 'UnderPage';
 }
 
 /// generated route for
-/// [_i7.NestedPage]
-class NestedPage extends _i8.PageRouteInfo<NestedPageArgs> {
-  NestedPage({_i9.Key? key})
+/// [_i7.HomePage]
+class HomePageRoute extends _i9.PageRouteInfo<void> {
+  const HomePageRoute() : super(HomePageRoute.name, path: '');
+
+  static const String name = 'HomePageRoute';
+}
+
+/// generated route for
+/// [_i8.NestedPage]
+class NestedPage extends _i9.PageRouteInfo<NestedPageArgs> {
+  NestedPage({_i10.Key? key})
       : super(NestedPage.name,
-            path: 'nested-page', args: NestedPageArgs(key: key));
+            path: 'nestedPage/*', args: NestedPageArgs(key: key));
 
   static const String name = 'NestedPage';
 }
@@ -160,7 +175,7 @@ class NestedPage extends _i8.PageRouteInfo<NestedPageArgs> {
 class NestedPageArgs {
   const NestedPageArgs({this.key});
 
-  final _i9.Key? key;
+  final _i10.Key? key;
 
   @override
   String toString() {
